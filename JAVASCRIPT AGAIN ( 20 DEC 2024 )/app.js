b0326b2d176733bdce9629e361d5edf49bbde0eb
Owais Ateeq <owaisatiq.7@gmail.com>
2025-01-05 12:46:00 +0500

    Add files via upload

---
+++ JAVASCRIPT AGAIN ( 20 DEC 2024 )/app.js
@@ -0,0 +1,138 @@
+// alert('Owais');
+// alert('Owais');
+// alert('Owais');
+// alert('Owais');
+// alert('Owais');
+
+// Arrays
+
+// var city1 = "karachi"
+// var city2 = "lahore"
+// var city3 = "islamabad"
+
+// console.log(city1)
+// console.log(city2)
+// console.log(city3)
+
+// var cities = [ "karachi" , "lahore" , "islamabad"  ]
+
+// console.log(cities)
+
+// var fruits = ["mango" , "apple"]
+
+// console.log(fruits[2])
+// console.log(cities[1])
+
+
+// var country = ["pakistan", " bangladesh", "turkey"]
+// country[3] = "arab"
+// country[2] = "muslim"
+// console.log(country)
+
+
+//   ... practise ...
+
+// var color = ["green", "white", " blue"]
+// color[3]  = "pink"
+// color[4] = "grey"
+// color[2] =  "silky"
+// console.log(color)
+
+
+
+// var cities = ["karachi", " lahore", "islamabad" ]
+
+// cities[1] = "islamabad"
+
+// console.log(cities.length)
+
+
+
+// var arr = [];
+
+// arr[0] = "karachi";
+// arr[2] = "lahore"
+// arr[1] = "islamabad"
+// console.log(arr[2]) 
+
+
+
+// var arr = ["karachi", 2025, 1993, 24, 101, 804, true, undefined, null];
+
+// console.log(arr.length);
+
+
+// ARRAY METHODS
+
+
+// pop , push ==> last  index
+
+// shift , unshift ==> first index
+
+// slice , splice
+
+
+// var cities = [ "karachi" , " lahore", "multan"];
+// cities.pop();
+// cities.push("Jaddah");
+
+// var cities = [ "karachi ", 'lahore']
+
+// cities.shift();
+// cities.unshift("islamabad")
+// console.log(cities)
+
+
+
+
+// var mobile = ["sams", " dell ", "huawei"]
+
+
+// var arrCopy = mobile.slice(0, -2);
+
+// var arrCopy = mobile.slice(0, 1);
+
+// var arrCopy = mobile.slice(0, -1);
+// var arrCopy = mobile.slice(-1);
+// var arrCopy = mobile.slice(0);
+// var arrCopy = mobile.slice(1);
+// console.log(arrCopy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
